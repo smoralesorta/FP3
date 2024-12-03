2da4b8a9ebadbbea9c40649629d8005aa46075d9
smoralesorta <seb_moralesorta@hotmail.com>
2024-12-02 19:34:05 -0600

    Create clase02dic.cpp

---
+++ clase02dic.cpp
@@ -0,0 +1,75 @@
+
+#include <iostream>
+using namespace std;
+//variables globales
+//estructuras /clase
+struct Alumno
+{
+	string nombre;
+	int cuenta;
+	//funciones de la estructura
+	static void altaAlumno()
+	{
+		Alumno alumno;
+	}
+};
+int main()
+{
+	Alumno alumno;
+	cout << "Nombre: ";
+	cin >> alumno.nombre;
+	cout << endl;
+	cout << "Cuenta: ";
+	cin >> alumno.cuenta;
+	cout << endl;
+	imprimeDatos(alumno)
+
+}
+static void imprimeDatos(Alumno alumno)
+{
+	cout << "Nombre: " << alumno.nombre << endl;
+	cout << "Cuenta: " << alumno.nombre << endl;
+
+}
+};
+int main()
+{
+	Alumno::altaAlumno 
+}
+#include <iostream>
+using namespace std;
+//variables globales
+//estructuras /clase
+struct Alumno
+{
+	string nombre;
+	int cuenta;
+	//funciones de la estructura
+	static void altaAlumno()
+	{
+		Alumno alumno;
+	}
+};
+int main()
+{
+	Alumno alumno;
+	cout << "Nombre: ";
+	cin >> alumno.nombre;
+	cout << endl;
+	cout << "Cuenta: ";
+	cin >> alumno.cuenta;
+	cout << endl;
+	imprimeDatos(alumno)
+
+}
+static void imprimeDatos(Alumno alumno)
+{
+	cout << "Nombre: " << alumno.nombre << endl;
+	cout << "Cuenta: " << alumno.nombre << endl;
+
+}
+};
+int main()
+{
+	Alumno::altaAlumno 
+}
